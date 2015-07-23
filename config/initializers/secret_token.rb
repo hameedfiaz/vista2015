9048057de5787c521caca8736da716d5213b1bd8
hameedfiaz <rmahameed005@gmail.com>
2015-07-23 08:45:16 +0530

    Initital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vista2015::Application.config.secret_token = '9bf90faa9a691e6276e06520f8bc82d5c869be285e8684ef80e4135a06f2d1fcfc531287f167a9575bf1e58e2d8440204506624da051e8f6d45e00af335d2352'
